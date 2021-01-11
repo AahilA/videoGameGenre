01d64fd0b8a9c5ffb77ed1b610ccdcf7c2f848be
AahilA <asa97@cornell.edu>
2021-01-10 23:29:12 -0500

    Resnet debugged

--- resnet.py
+++ resnet.py
@@ -7,12 +7,12 @@ class block(nn.Module):
     def __init__(self, in_chan, out_chan, stride=1):
         super(block,self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_chan, out_chan, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_chan, out_chan, kernel_size=3, stride=stride, padding=1, bias=False),
             nn.BatchNorm2d(out_chan)
         )
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(out_chan, out_chan, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(out_chan, out_chan, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_chan)
         )
 
@@ -21,7 +21,7 @@ class block(nn.Module):
         self.shortcut = nn.Sequential()
         if stride != 1 or in_chan != out_chan:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_chan, out_chan, kernel_size=1, stride=stride),
+                nn.Conv2d(in_chan, out_chan, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_chan)
             )
 
@@ -30,19 +30,21 @@ class block(nn.Module):
         out = self.conv2(out)
         out += self.shortcut(x)
         out = self.relu(out)
-        return x
+        return out
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_classes):
+    def __init__(self, block, num_classes=10):
         super(ResNet, self).__init__()
+
         self.input_chan = 64
+
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=1),
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(64)
         )
 
         self.relu = nn.ReLU()
-        self.max_pool = nn.MaxPool2d(3,3) #wtf these numbers be
+        self.max_pool = nn.MaxPool2d(3,2) #wtf these numbers be
 
         #Resnet Layers
         self.layer1 = self._make_layer(block, 3, 64, 1)
@@ -50,7 +52,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 6, 256, 2)
         self.layer4 = self._make_layer(block, 3, 512, 2)
 
-        self.avg_pool = nn.AvgPool2d(1,1)
+        self.avg_pool = nn.AvgPool2d(4)
         self.fc = nn.Linear(512, num_classes)
 
 
@@ -61,13 +63,14 @@ class ResNet(nn.Module):
         self.input_chan = out_chan
 
         for i in range(num_blocks - 1):
-            layers.append(block(self.input_chan,out_chan,stride))
+            layers.append(block(self.input_chan,out_chan, 1))
 
         return nn.Sequential(*layers)
     
     #TODO ADD POOLING
     def forward(self, x):
-        x = self.relu(self.conv1(x))
+        x = self.conv1(x)
+        x = self.relu(x)
         x = self.max_pool(x)
 
         #Resnet
@@ -78,6 +81,16 @@ class ResNet(nn.Module):
 
         x = self.avg_pool(x)
         x = x.reshape(x.shape[0], -1)
+
         x = self.fc(x)
 
         return x
+
+
+def test():
+    net = ResNet(block)
+    y = net(torch.randn(1, 3, 128, 128))
+    print(y.size())
+    print(y)
+
+# test()
