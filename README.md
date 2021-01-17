041395a8dd4186656fd2655f66536e0033b94f9f
ZhengPeterWang <zw494@cornell.edu>
2021-01-17 17:39:07 +0000

    updated readme and test

--- README.md
+++ README.md
@@ -10,5 +10,6 @@ The model is a ResNet 34 network. It was implemented in PyTorch.
 
 ## Training and Testing
 
+The dataset we get is first converted to RGB format if they are not, with 3 layers, and are resized to size 256 * 256. We then split the dataset into two sets: 80% are the training set and 20% are the testing set. We then train the model using SGD with momentum 0.9 and L1 loss. We then trained with 100 epoches using the ResNet 34 network we described above. Training and testing accuracies are calculated by dividing all correct classifications of genres by the total amount of classifications. 
 
 
--- test.py
+++ test.py
@@ -6,39 +6,74 @@ import numpy as np
 import resnet
 
 gameLabels = pd.read_csv("gameLabels.csv", header=None)
+thres = 0.5
+tot = 150
+gen = 65
+tn = [0 for i in range(gen)]
+tp = [0 for i in range(gen)]
+fn = [0 for i in range(gen)]
+fp = [0 for i in range(gen)]
 
-img_no = 12
+totnum = 0
 
-img_name = 'game_images/' + gameLabels.iloc[img_no,0] + '.jpg'
-image = io.imread(img_name)
-label = gameLabels.iloc[img_no, 2:]
-name = gameLabels.iloc[img_no, 1]
-label = np.array([label])
-label = label.astype('float').reshape(-1, 65).flatten()
+for img_no in range(tot):
+    print("image: " + str(img_no))
 
-image = transform.resize(image, (256,256))
-
-image = image[..., None]
-
-image = torch.from_numpy(image.transpose((3, 2, 0, 1)))
-
-
-model = resnet.ResNet(resnet.block, 65).float()
-model.load_state_dict(torch.load('gameModel'))
-
-output = model(image.float())
-b = output.clone()
-
-for i in range(0,65):
-    if output[0][i] >= 0.5:
-        output[0][i] = 1
+    img_name = 'game_images/' + gameLabels.iloc[img_no,0] + '.jpg'
+    image = io.imread(img_name)
+    label = gameLabels.iloc[img_no, 2:]
+    name = gameLabels.iloc[img_no, 1]
+    label = np.array([label])
+    label = label.astype('float').reshape(-1, gen).flatten()
+    image = transform.resize(image, (256,256))
+    
+    image = image[..., None]
+    print(np.array(image).shape)
+    if len(np.array(image).shape) != 4:
+        continue
     else:
-        output[0][i] = 0
+        totnum += 1
+    image = torch.from_numpy(image.transpose((3, 2, 0, 1)))
+    
+    
+    model = resnet.ResNet(resnet.block, gen).float()
+    model.load_state_dict(torch.load('gameModel'))
+    
+    output = model(image.float())
+    b = output.clone()
+    
 
-print(name)
-print(label)
-print(output)
-print(b)
+    for i in range(0,gen):
+        if output[0][i] >= thres:
+            output[0][i] = 1
+        else:
+            output[0][i] = 0
+        if output[0][i] == 1 and label[i] == 1:
+            tp[i] += 1
+        if output[0][i] == 1 and label[i] == 0:
+            fp[i] += 1
+        if output[0][i] == 0 and label[i] == 1:
+            fn[i] += 1
+        if output[0][i] == 0 and label[i] == 0:
+            tn[i] += 1
+
+#    print(name)
+#    print(label)
+#    print(output)
+#    print(b)
 
 
+tp = [tp[i] * 1.0 / totnum for i in range(gen)]
+tn = [tn[i] * 1.0 / totnum for i in range(gen)]
+fp = [fp[i] * 1.0 / totnum for i in range(gen)]
+fn = [fn[i] * 1.0 / totnum for i in range(gen)]
+tpr = [((tp[i] / (tp[i] + fn[i])) if ((tp[i] + fn[i]) != 0.) else 0.) for i in range(gen)]
+fpr = [((fp[i] / (tn[i] + fp[i])) if ((tn[i] + fp[i]) != 0.) else 0.) for i in range(gen)]
+
+print(tp)
+print(tn)
+print(fp)
+print(fn)
+print(tpr)
+print(fpr)
 
